d28a8988d1a23a77e19e8e1daed40d75d7e09007
Maxim Bublis <b@codemonkey.ru>
2016-01-18 04:45:21 -0800

    Add exercise 1.1.7 solution.

---
+++ exercises/1.1/Ex_1_1_7.java
@@ -0,0 +1,39 @@
+// Copyright (C) 2016  Maxim Bublis
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex_1_1_7
+{
+    public static void main(String[] args)
+    {
+        double t = 9.0;
+        while (Math.abs(t - 9.0/t) > .001)
+            t = (9.0/t + t) / 2.0;
+        StdOut.printf("a. %.5f\n", t);
+
+        int sum = 0;
+        for (int i = 1; i < 1000; i++)
+            for (int j = 0; j < i; j++)
+                sum++;
+        StdOut.printf("b. %d\n", sum);
+
+        sum = 0;
+        for (int i = 1; i < 1000; i *= 2)
+            for (int j = 0; j < 1000; j++)
+                sum++;
+        StdOut.printf("c. %d\n", sum);
+    }
+}
