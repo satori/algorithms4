991bf16f3ab4ad6723f282c206f61a2e56520c8f
Maxim Bublis <b@codemonkey.ru>
2016-01-19 01:59:29 -0800

    Add exercise 1.1.12 solution.

---
+++ exercises/1.1/Ex_1_1_12.java
@@ -0,0 +1,28 @@
+// Copyright (C) 2016  Maxim Bublis
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+public class Ex_1_1_12
+{
+    public static void main(String[] args)
+    {
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++)
+            a[i] = 9 - i;
+        for (int i = 0; i < 10; i++)
+            a[i] = a[a[i]];
+        for (int i = 0; i < 10; i++)
+            System.out.println(a[i]);
+    }
+}
