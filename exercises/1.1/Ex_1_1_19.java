0a0aedaf9279c0d0400d44ef4ec9fa5b4ce238ad
Maxim Bublis <b@codemonkey.ru>
2016-01-19 23:12:21 -0800

    Add exercise 1.1.19 solution.

---
+++ exercises/1.1/Ex_1_1_19.java
@@ -0,0 +1,40 @@
+// Copyright (C) 2016  Maxim Bublis
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex_1_1_19
+{
+    private static long[] cached;
+
+    public static long F(int N)
+    {
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+
+        if (cached[N] == 0)
+            cached[N] = F(N - 1) + F(N - 2);
+
+        return cached[N];
+    }
+
+    public static void main(String[] args)
+    {
+        cached = new long[90];
+
+        for (int N = 0; N < 90; N++)
+            StdOut.println(N + " " + F(N));
+    }
+}
