a15555e6614a881e8c2e0cd985b07b311451d4ec
Maxim Bublis <b@codemonkey.ru>
2016-01-19 22:45:14 -0800

    Add exercise 1.1.16 solution.

---
+++ exercises/1.1/Ex_1_1_16.java
@@ -0,0 +1,31 @@
+// Copyright (C) 2016  Maxim Bublis
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex_1_1_16
+{
+    public static String exR1(int n)
+    {
+        if (n <= 0) return "";
+        return exR1(n - 3) + n + exR1(n - 2) + n;
+    }
+
+    public static void main(String[] args)
+    {
+        assert exR1(6) == "311361142246";
+        StdOut.printf("exR1(6) = %s\n", exR1(6));
+    }
+}
